61e27adf65f0c89db3fb3d237c1438d5363dfdcd
OdilonLimaNeto <odilongeronimoo@gmail.com>
2021-06-24 23:53:30 -0500

    Merge - Authentication finalized

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,19 +1,13 @@
 import { Router } from "express";
 
-<<<<<<< HEAD
-import { tagRoutes } from "./tag.routes";
-=======
 import { authenticateRoutes } from "./authenticate.routes";
->>>>>>> aplicacoes
+import { tagRoutes } from "./tag.routes";
 import { userRoutes } from "./users.routes";
 
 const router = Router();
 
 router.use("/user", userRoutes);
-<<<<<<< HEAD
 router.use("/tags", tagRoutes);
-=======
 router.use(authenticateRoutes);
->>>>>>> aplicacoes
 
 export { router };
--- src/shared/infra/http/routes/tag.routes.ts
+++ src/shared/infra/http/routes/tag.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { CreateTagController } from "../../../../modules/tag/useCases/CreateTag/CreateTagController";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const tagRoutes = Router();
 
 const createTagController = new CreateTagController();
 
-tagRoutes.post("/", createTagController.handle);
+tagRoutes.use(ensureAuthenticated);
+tagRoutes.post("/", ensureAdmin, createTagController.handle);
 
 export { tagRoutes };
